788f2bcb8a2a79a4e6618bf349a230a8b6d0354d
Aline Ribeiro <alinear.mail@gmail.com>
2021-06-15 15:01:46 -0700

    Style background and header

---
+++ src/components/authentication/LogInPage.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import styled from '@emotion/styled'
+import { Box, Text } from "@chakra-ui/react";
+
+
+export const LogInPage = () => {
+  return (
+    <PageBackground >
+      <Image>
+
+      </Image>
+        <FormBackground>
+          <FormContainer>
+            <Header>Login To Your Account</Header>
+          </FormContainer>
+
+        </FormBackground>
+    </PageBackground>
+  );
+}
+
+const Image = styled(Box)`
+width: 70%;
+`
+
+const PageBackground = styled(Box)`
+background-color: rgb(49, 81, 254);
+height: 100vh;
+display: flex;
+`
+
+const FormContainer = styled(Box)`
+padding-top: 25%;
+`
+
+const FormBackground = styled(Box)`
+background-color: white;
+border-radius: 12px; 
+width: 30%;
+height: 90%;
+margin-top: 32px;
+margin-right: 56px;
+`
+
+const Header = styled(Text)`
+font-weight: 700;
+font-size: 24px;
+text-align: center;
+`
