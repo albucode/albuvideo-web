0311d53851fcd097e02506694fb9090607718c56
Aline Ribeiro <alinear.mail@gmail.com>
2021-06-21 14:33:42 -0700

    Import tabs icons

--- src/features/navigation/SideBar.jsx
+++ src/features/navigation/SideBar.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Speedometer from "../shared/icons/Speedometer";
+import Video from "../shared/icons/Video";
+import Ticket from "../shared/icons/Ticket";
 import theme from "../../theme/theme";
+import DoubleCheck from "../shared/icons/DoubleCheck";
 
 export const SideBar = () => {
   return (
     <>
       <Speedometer color={theme.colors.grey1} />
+      <Video color={theme.colors.grey1} />
+      <Ticket color={theme.colors.grey1} />
+      <DoubleCheck color={theme.colors.grey1} />
     </>
   );
 };
