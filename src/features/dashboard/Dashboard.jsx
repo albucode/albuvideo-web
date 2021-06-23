58712714829db35cde98888442e29763c7abbcac
Aline Ribeiro <alinear.mail@gmail.com>
2021-06-23 16:50:23 -0700

    Align and center Dashboard content

--- src/features/dashboard/Dashboard.jsx
+++ src/features/dashboard/Dashboard.jsx
@@ -1,11 +1,30 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
+import styled from "@emotion/styled";
+import { Box, Text } from "@chakra-ui/react";
+
 import { TopBar } from "../navigation/TopBar";
 
 export const Dashboard = () => {
   return (
-    <Box>
+    <Box margin="0 auto">
       <TopBar sectionName="Dashboard" />
+      <ContentContainer>
+        <Title>Content</Title>
+      </ContentContainer>
     </Box>
   );
 };
+
+
+const ContentContainer = styled(Box)`
+  background-color: ${(props) => props.theme.colors.white};
+  height: 300px;
+  border-radius: 20px;
+  padding: 32px;
+  `;
+
+const Title = styled(Text)`
+  color: ${(props) => props.theme.colors.black};
+  font-weight: 700;
+  font-size: 24px;
+`;
