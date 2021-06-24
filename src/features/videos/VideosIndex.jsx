fb5f363c662eae63dd972fa2658012b8745b6fb7
Aline Ribeiro <alinear.mail@gmail.com>
2021-06-24 15:56:45 -0700

    Draft building

--- src/features/videos/VideosIndex.jsx
+++ src/features/videos/VideosIndex.jsx
@@ -1,9 +1,61 @@
 import React from "react";
+import styled from "@emotion/styled";
+import { Box, Table, Tfoot, Thead, Tr, Td, Th, Tbody } from "@chakra-ui/react";
+
+import { TopBar } from "../navigation/TopBar";
 
 export const VideosIndex = () => {
   return (
-    <>
-      <p>This is the Videos Index Page</p>
-    </>
+    <Box margin="0 auto" paddingTop="50px">
+      <TopBar sectionName="Videos" />
+      <Table
+        variant="striped"
+        colorScheme="table"
+        backgroundColor="white"
+        marginRight="0px"
+        borderRadius="12px"
+      >
+        <Thead>
+          <Tr>
+            <THeader>Title</THeader>
+            <THeader>Status</THeader>
+            <THeader>Created at</THeader>
+            <THeader>Duration</THeader>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr>
+            <Td>Video title 1</Td>
+            <Td>Processing</Td>
+            <Td>December 27th, 2021 04:56 AM</Td>
+            <Td>6:10</Td>
+          </Tr>
+          <Tr>
+            <Td>Video title 1</Td>
+            <Td>Processing</Td>
+            <Td>December 27th, 2021 04:56 AM</Td>
+            <Td>6:10</Td>
+          </Tr>
+          <Tr>
+            <Td>Video title 1</Td>
+            <Td>Processing</Td>
+            <Td>December 27th, 2021 04:56 AM</Td>
+            <Td>6:10</Td>
+          </Tr>
+        </Tbody>
+        <Tfoot>
+          <Tr>
+            <Th></Th>
+          </Tr>
+        </Tfoot>
+      </Table>
+    </Box>
   );
 };
+
+const THeader = styled(Th)`
+  text-transform: capitalize;
+  font-size: 18px;
+  font-weight: 400;
+  color: ${(props) => props.theme.colors.grey1};
+`;
--- src/theme/theme.js
+++ src/theme/theme.js
@@ -6,10 +6,14 @@ const theme = extendTheme({
     body: "Open Sans",
   },
   colors: {
+    table: {
+       100: "#F3F2F7",
+    },
     lightBlue: "rgb(49, 81, 254)",
     black: "#222222",
     blue: "#3246D2",
     grey1: "#98A1B0",
+    grey2: "#F3F2F7",
     grey6: "#EDF1F5",
     grey7: "#E5E5E5",
   },
