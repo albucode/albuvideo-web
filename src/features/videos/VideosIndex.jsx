fd19248e1042a0e3873c73fba3fbabb72d9e0740
Aline Ribeiro <alinear.mail@gmail.com>
2021-06-25 15:13:19 -0700

    Import dots and fix padding

--- src/features/videos/VideosIndex.jsx
+++ src/features/videos/VideosIndex.jsx
@@ -10,13 +10,14 @@ import {
   Th,
   Tbody,
   Text,
-  Flex,
+  HStack,
 } from "@chakra-ui/react";
 
 import theme from "../../../src/theme/theme";
 import { TopBar } from "../navigation/TopBar";
 import Play from "../shared/icons/Play";
 import { Thumbnail } from "../shared/Thumbnail";
+import Dots from "../shared/icons/Dots";
 
 export const VideosIndex = () => {
   return (
@@ -34,32 +35,22 @@ export const VideosIndex = () => {
             <THeader>Title</THeader>
             <THeader>Status</THeader>
             <THeader>Created at</THeader>
-            <THeader>Duration</THeader>
+            <THeader pr={0}>Duration </THeader>
+            <THeader pr={0} >{""}</THeader>
           </Tr>
         </Thead>
         <Tbody>
           <Tr>
             <Td>
-              <Flex>
+              <HStack>
                 <Thumbnail icon={<Play />} inputColor={theme.colors.magenta} />
-                <Text>Video title 1</Text>
-              </Flex>
+                <VideoTitle>Video title 1</VideoTitle>
+              </HStack>
             </Td>
-            <Td>Processing</Td>
-            <Td>December 27th, 2021 04:56 AM</Td>
-            <Td>6:10</Td>
-          </Tr>
-          <Tr>
-            <Td>Video title 1</Td>
-            <Td>Processing</Td>
-            <Td>December 27th, 2021 04:56 AM</Td>
-            <Td>6:10</Td>
-          </Tr>
-          <Tr>
-            <Td>Video title 1</Td>
-            <Td>Processing</Td>
-            <Td>December 27th, 2021 04:56 AM</Td>
-            <Td>6:10</Td>
+            <TData>Processing</TData>
+            <TData>December 27th, 2021 04:56 AM</TData>
+            <TData pr={0}>6:10</TData>
+            <TData pl={0}><Dots color={theme.colors.grey1} /></TData>
           </Tr>
         </Tbody>
         <Tfoot>
@@ -77,4 +68,16 @@ const THeader = styled(Th)`
   font-size: 18px;
   font-weight: 400;
   color: ${(props) => props.theme.colors.grey1};
+`;
+
+const TData = styled(Td)`
+  font-size: 14px;
+  color: ${(props) => props.theme.colors.black};
+`;
+
+const VideoTitle = styled(Text)`
+  font-size: 14px;
+  color: ${(props) => props.theme.colors.black};
+  font-weight: 700;
+  padding-left: 18px;
 `;
