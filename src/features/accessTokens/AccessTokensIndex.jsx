dd1a3f1e49c174bf04ee194a28671d3f6211c8ea
Aline Ribeiro <alinear.mail@gmail.com>
2021-06-28 11:53:16 -0700

    Draft accessTokens indes

--- src/features/accessTokens/AccessTokensIndex.jsx
+++ src/features/accessTokens/AccessTokensIndex.jsx
@@ -5,6 +5,10 @@ import { PageContainer } from "../shared/PageContainer";
 import { TopBar } from "../navigation/TopBar";
 import { AccessTokens } from "../../api/requests";
 import { loadAccessTokens } from "./accessTokenSlice";
+import { Table, Tbody, Td, Text, Tfoot, Th, Thead, Tr } from "@chakra-ui/react";
+import theme from "../../theme/theme";
+import Dots from "../shared/icons/Dots";
+import styled from "@emotion/styled";
 
 export const AccessTokensIndex = () => {
   const dispatch = useDispatch();
@@ -38,6 +42,58 @@ export const AccessTokensIndex = () => {
   return (
     <PageContainer>
       <TopBar sectionName="Access Tokens" />
+      <Table
+        variant="striped"
+        colorScheme="table"
+        backgroundColor="white"
+        marginRight="0px"
+        borderRadius="12px"
+      >
+        <Thead>
+          <Tr>
+            <THeader>Name</THeader>
+            <THeader pr={0}>Created at</THeader>
+            <THeader pl={0}>{""}</THeader>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {accessTokens.map((token) => (
+            <Tr key={token.id}>
+              <Td>
+                <VideoTitle>{token.name}</VideoTitle>
+              </Td>
+              <TData pr={0}>{formatDate(token.created_at)}</TData>
+              <TData pl={0}>
+                <Dots color={theme.colors.grey1} />
+              </TData>
+            </Tr>
+          ))}
+        </Tbody>
+        <Tfoot>
+          <Tr>
+            <Th></Th>
+          </Tr>
+        </Tfoot>
+      </Table>
     </PageContainer>
   );
 };
+
+const THeader = styled(Th)`
+  text-transform: capitalize;
+  font-size: 18px;
+  font-weight: 400;
+  color: ${(props) => props.theme.colors.grey1};
+`;
+
+const TData = styled(Td)`
+  font-size: 14px;
+  color: ${(props) => props.theme.colors.black};
+`;
+
+const VideoTitle = styled(Text)`
+  font-size: 14px;
+  color: ${(props) => props.theme.colors.black};
+  font-weight: 700;
+  padding-left: 18px;
+`;
