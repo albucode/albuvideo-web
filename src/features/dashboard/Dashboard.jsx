c1fd4ceb3df72e0bbd54567800bdc6a71c48fe6d
Aline Ribeiro <alinear.mail@gmail.com>
2021-07-20 11:01:01 -0700

    Render icon tag draft

--- src/features/dashboard/Dashboard.jsx
+++ src/features/dashboard/Dashboard.jsx
@@ -4,6 +4,9 @@ import { Box, Text } from "@chakra-ui/react";
 
 import { PageContainer } from "../shared/PageContainer";
 import { TopBar } from "../navigation/TopBar";
+import { Stats } from "./Stats";
+import Users from "../shared/icons/Users";
+import theme from "../../theme/theme";
 
 export const Dashboard = () => {
   return (
@@ -12,6 +15,14 @@ export const Dashboard = () => {
       <ContentContainer>
         <Title>Content</Title>
       </ContentContainer>
+      <StatsContainer>
+        <Stats
+          title={"Total streamed"}
+          data={"23h 43m"}
+          icon={<Users />}
+          inputColor={theme.colors.cyan}
+        />
+      </StatsContainer>
     </PageContainer>
   );
 };
@@ -21,6 +32,13 @@ const ContentContainer = styled(Box)`
   height: 300px;
   border-radius: 20px;
   padding: 32px;
+`;
+
+const StatsContainer = styled(Box)`
+  background-color: ${(props) => props.theme.colors.white};
+  border-radius: 20px;
+  padding: 32px;
+  margin-top: 30px;
 `;
 
 const Title = styled(Text)`
