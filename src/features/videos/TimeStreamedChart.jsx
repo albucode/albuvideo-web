18f99f609cf83e1a47be12a6461c7bc6c9cbe767
Aline Ribeiro <alinear.mail@gmail.com>
2021-07-27 16:02:19 -0700

    Use request data to render chart

--- src/features/videos/TimeStreamedChart.jsx
+++ src/features/videos/TimeStreamedChart.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
 import {
   VictoryBar,
   VictoryChart,
@@ -7,65 +9,41 @@ import {
   VictoryLegend,
   VictoryTooltip,
 } from "victory";
+
 import theme from "../../theme/theme";
 
 export const TimeStreamedChart = () => {
-  const data = [
-    { hour: 1, time_streamed: 130 },
-    { hour: 2, time_streamed: 1425 },
-    { hour: 3, time_streamed: 1900 },
-    { hour: 4, time_streamed: 130 },
-    { hour: 5, time_streamed: 165 },
-    { hour: 6, time_streamed: 1425 },
-    { hour: 7, time_streamed: 1900 },
-    { hour: 8, time_streamed: 130 },
-    { hour: 9, time_streamed: 165 },
-    { hour: 10, time_streamed: 1425 },
-    { hour: 11, time_streamed: 1900 },
-    { hour: 12, time_streamed: 1900 },
-    { hour: 13, time_streamed: 130 },
-    { hour: 14, time_streamed: 1425 },
-    { hour: 15, time_streamed: 1900 },
-    { hour: 16, time_streamed: 130 },
-    { hour: 17, time_streamed: 165 },
-    { hour: 18, time_streamed: 1425 },
-    { hour: 19, time_streamed: 1900 },
-    { hour: 20, time_streamed: 130 },
-    { hour: 21, time_streamed: 165 },
-    { hour: 22, time_streamed: 1425 },
-    { hour: 23, time_streamed: 1900 },
-    { hour: 24, time_streamed: 1900 },
-  ];
+  const data = useSelector((state) => state.video.videoStats);
 
   return (
     <VictoryChart domainPadding={{ x: 8 }}>
       <VictoryLegend
         x={330}
         y={0}
-        data={[{ name: "hours", symbol: { fill: theme.colors.cyan } }]}
+        data={[{ name: "seconds", symbol: { fill: theme.colors.cyan } }]}
       />
       <VictoryLabel text="Time streamed" x={50} y={10} textAnchor="middle" />
       <VictoryAxis
-        tickValues={[1, 8, 16, 24]}
-        tickFormat={["1am", "8am", "4pm", "12am"]}
+        // tickValues={[1, 8, 16, 24]}
+        // tickFormat={["1am", "8am", "4pm", "12am"]}
         style={{
           grid: { stroke: theme.colors.grey2, strokeWidth: 0.5 },
         }}
       />
       <VictoryAxis
         dependentAxis
-        tickFormat={[100, 500, 1000, 1500, 2000]}
+        tickFormat={[100000, 50000, 100000]}
         style={{
           grid: { stroke: theme.colors.grey2, strokeWidth: 0.9 },
         }}
       />
       <VictoryBar
         data={data}
-        x="hour"
-        y="time_streamed"
+        y="sum"
+        x="period"
         style={{ data: { fill: theme.colors.cyan, width: 6 } }}
         labels={({ datum }) =>
-          `Time streamed: ${datum.time_streamed}  \n Hour:${datum.hour}`
+          `Time streamed: ${datum.sum}  \n Hour:${datum.period}`
         }
         labelComponent={<VictoryTooltip />}
       />
