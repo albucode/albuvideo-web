2e31a44850ef6e04240f943d455d57d1b6e13052
Aline Ribeiro <alinear.mail@gmail.com>
2021-08-04 11:06:20 -0700

    Refactor switch statement

--- src/utils/statusToColor.js
+++ src/utils/statusToColor.js
@@ -4,10 +4,8 @@ const statusToColor = (videoStatus) => {
   switch (videoStatus) {
     default:
       return theme.colors.cyan;
-      break;
     case "ready":
       return theme.colors.blue;
-      break;
     case "failed":
       return theme.colors.red;
   }
