d76034183fd014215392d1af234e42cbbea31945
Aline Ribeiro <alinear.mail@gmail.com>
2021-08-12 12:41:27 -0700

    Draft form

--- src/features/videos/VideoCreate.jsx
+++ src/features/videos/VideoCreate.jsx
@@ -1,11 +1,31 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { FormLabel, Input, Button } from "@chakra-ui/react";
+
 import { TopBar } from "../navigation/TopBar";
 import { PageContainer } from "../shared/PageContainer";
 
 const VideoCreate = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
   return (
     <PageContainer>
       <TopBar sectionName="New video" />
+      <form
+        onSubmit={handleSubmit((data) => {
+          console.log(data);
+        })}
+      >
+        <FormLabel>Title</FormLabel>
+        <Input {...register("title")} />
+        <Input {...register("source", { required: "Source is required" })} />
+        {errors.source && <p>{errors.source.message}</p>}
+        <Button type="submit">Submit</Button>
+      </form>
     </PageContainer>
   );
 };
