00c1bf4356509075eaa1ad138b825173d5cc3fdd
Aline Ribeiro <alinear.mail@gmail.com>
2021-09-15 15:49:29 -0700

    Render video

--- src/features/videos/VideoPreview.jsx
+++ src/features/videos/VideoPreview.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import ReactHlsPlayer from "react-hls-player";
 import { VideoHeader } from "./VideoHeader";
 import { PageContainer } from "../shared/PageContainer";
 import { TopBar } from "../navigation/TopBar";
+import { StatsContainer } from "../shared/StatsContainer";
 
 export const VideoPreview = () => {
   return (
@@ -9,6 +11,15 @@ export const VideoPreview = () => {
       <PageContainer>
         <TopBar sectionName="Video" />
         <VideoHeader />
+        <StatsContainer>
+          <ReactHlsPlayer
+            src="http://localhost:8000/videos/buqSlPHmvd.m3u8"
+            autoPlay={false}
+            controls={true}
+            width="100%"
+            height="auto"
+          />
+        </StatsContainer>
       </PageContainer>
     </>
   );
