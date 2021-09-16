9b29da466aed2ee397ada62997a1250f7f66f3ba
Aline Ribeiro <alinear.mail@gmail.com>
2021-09-16 12:16:40 -0700

    Add BackToVideo button

--- src/features/videos/VideoPreview.jsx
+++ src/features/videos/VideoPreview.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import ReactHlsPlayer from "react-hls-player";
 import { VideoHeader } from "./VideoHeader";
 import { PageContainer } from "../shared/PageContainer";
 import { TopBar } from "../navigation/TopBar";
-import { StatsContainer } from "../shared/StatsContainer";
 import { Video } from "../../api/requests";
 import { useParams } from "react-router-dom";
+import styled from "@emotion/styled";
+import theme from "../../theme/theme";
+import { Button, Box } from "@chakra-ui/react";
 
 export const VideoPreview = () => {
   const [video, setVideo] = useState({});
@@ -30,7 +33,10 @@ export const VideoPreview = () => {
             playlistUrl={video.playlist_url}
             status={video.status}
           />
-          <StatsContainer>
+          <Container>
+            <Link to={`/videos/${videoId}`}>
+              <BackButton>Back to video</BackButton>
+            </Link>
             <ReactHlsPlayer
               src={video.playlist_url}
               autoPlay={false}
@@ -38,9 +44,28 @@ export const VideoPreview = () => {
               width="100%"
               height="auto"
             />
-          </StatsContainer>
+          </Container>
         </PageContainer>
       )}
     </>
   );
 };
+
+const BackButton = styled(Button)`
+  background-color: ${theme.colors.cyan};
+  margin-bottom: 16px;
+  color: white;
+  width: 150px;
+  height: 45px;
+  font-weight: 700;
+  font-size: 18px;
+`;
+
+const Container = styled(Box)`
+  background-color: ${(props) => props.theme.colors.white};
+  border-radius: 20px;
+  padding: 32px;
+  margin-bottom: 30px;
+  display: flex;
+  flex-direction: column;
+`;
