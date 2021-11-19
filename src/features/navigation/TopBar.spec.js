1b64f5c790a8a4db7d259eee97eb111f62c54eac
Aline Ribeiro <alinear.mail@gmail.com>
2021-11-19 14:30:13 -0800

    Test TopBar snapshot

--- src/features/navigation/TopBar.spec.js
+++ src/features/navigation/TopBar.spec.js
@@ -5,39 +5,41 @@ import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../authentication/userSlice";
 
-describe("TopBar", () => {
-  beforeEach(() => {
-    const store = configureStore({
-      reducer: {
-        user: userReducer,
+const fakeRenderComponent = () => {
+  const store = configureStore({
+    reducer: {
+      user: userReducer,
+    },
+    preloadedState: {
+      user: {
+        email: "test@test.com",
+        emailInitial: "T",
+        isLoggedIn: true,
       },
-      preloadedState: {
-        user: {
-          email: "a@a.com",
-          emailInitial: "A",
-          isLoggedIn: true,
-        },
-      },
-    });
-
-    render(
-      <Provider store={store}>
-        <TopBar sectionName={"Test"} />
-      </Provider>
-    );
+    },
   });
 
+  const { container } = render(
+    <Provider store={store}>
+      <TopBar sectionName={"Test"} />
+    </Provider>
+  );
+  return { container };
+};
+
+describe("TopBar", () => {
   it("displays section name", async () => {
+    fakeRenderComponent();
     expect(screen.getByTestId("section-name")).toHaveTextContent("Test");
   });
 
-  it("displays section name styles", async () => {
-    expect(screen.getByTestId("section-name")).toHaveStyle(
-      "color: #222222; width: 345px; font-weight: 700; font-size: 36px; line-height: 49px;"
-    );
+  it("displays email initial", async () => {
+    fakeRenderComponent();
+    expect(screen.getByTestId("email-initial")).toHaveTextContent("T");
   });
 
-  it("displays email initial", async () => {
-    expect(screen.getByTestId("email-initial")).toHaveTextContent("A");
+  it("matches snapshot", () => {
+    const { container } = fakeRenderComponent();
+    expect(container).toMatchSnapshot();
   });
 });
